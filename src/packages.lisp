b4f0e901a2039f92482ca2ba7a12ed9b5d4b7b67
Chris Schafmeister <meister@temple.edu>
2019-02-20 21:00:58 -0500

    Add packages.lisp

---
+++ src/packages.lisp
@@ -0,0 +1,40 @@
+(defpackage :netcdf-cffi
+        (:nicknames :nc-c)
+        (:use :cffi))
+
+(defpackage :netcdf
+  (:nicknames :nc)
+  (:use :cl)
+  (:export
+   nc-open
+   nc-close
+   get-dimension
+   copy-array-to-cffi
+   copy-array-from-cffi
+   create
+   def-dim
+   def-var
+   enddef
+   put-var-int
+   put-var-double
+   put-vara-int
+   put-vara-double
+   put-att-text
+   #:xtype
+   #:len
+   #:ndims
+   #:dimids
+   #:natts
+   #:attribute
+   #:variable
+   #:id
+   #:dimensions
+   #:variables
+   #:global-attributes
+   #:len
+   #:netcdf
+   #:varid
+   #:get-vara-double-static
+   #:get-vara-float
+   ))
+
